5f7db02af807a6cd7a45b925b39677c49e611747
castrintt <igordc38@gmail.com>
2026-05-12 21:47:12 -0300

    Refactor Opening schema and introduce OpeningResponse type for API responses

--- goportunities/schemas/opening.go
+++ goportunities/schemas/opening.go
@@ -1,15 +1,29 @@
 package schemas
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type Opnening struct {
 	gorm.Model
-	Role     string `json:"role"`
-	Company  string `json:"company"`
-	Location string `json:"location"`
-	Remote   bool   `json:"remote"`
-	Link     string `json:"link"`
-	Salary   int64  `json:"salary"`
+	Role     string
+	Company  string
+	Location string
+	Remote   bool
+	Link     string
+	Salary   int64
+}
+
+type OpeningResponse struct {
+	ID        uint      `json:"id"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    bool      `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int64     `json:"salary"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
