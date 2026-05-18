2165260f18fa3891b568b02c14501f66f55604fd
castrintt <igordc38@gmail.com>
2026-05-18 18:13:00 -0300

    Implement FindAllOpeningHandler with database retrieval and default message handling; return success or error responses based on query results

--- goportunities/handler/findAllOpening.go
+++ goportunities/handler/findAllOpening.go
@@ -1,13 +1,19 @@
 package handler
 
 import (
+	"goportunities/handler/defaultMessages"
+	"goportunities/schemas"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func FindAllOpeningHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "hello workd!",
-	})
+	var openings []schemas.Opening
+	if err := db.Find(&openings).Error; err != nil {
+		defaultMessages.SendError(c, http.StatusInternalServerError, "Failed to find all openings")
+		return
+	}
+
+	defaultMessages.SendSuccess(c, "Openings found successfully", openings)
 }
