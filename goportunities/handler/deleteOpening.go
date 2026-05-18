4ab4fa0f9c15053fa9de48e5dc5d4d4e65e96f2c
castrintt <igordc38@gmail.com>
2026-05-18 17:51:30 -0300

    Implement DeleteOpeningHandler with error handling and validation; ensure proper ID checks and success messages

--- goportunities/handler/deleteOpening.go
+++ goportunities/handler/deleteOpening.go
@@ -1,13 +1,35 @@
 package handler
 
 import (
+	"goportunities/handler/defaultMessages"
+	"goportunities/schemas"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
-func DeleteOpeningHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "hello workd!",
-	})
+func DeleteOpeningHandler(context *gin.Context) {
+	paramId := context.Param("id")
+
+	if paramId == "" {
+		defaultMessages.SendError(context, http.StatusBadRequest, "ID is required")
+		return
+	}
+
+	id64, err := strconv.ParseUint(paramId, 10, 64)
+
+	if err != nil {
+		defaultMessages.SendError(context, http.StatusBadRequest, "Invalid ID")
+		return
+	}
+
+	id := uint(id64)
+
+	if err := db.Delete(&schemas.Opening{}, id).Error; err != nil {
+		defaultMessages.SendError(context, http.StatusInternalServerError, "Failed to delete opening")
+		logger.Errorf("ERROR DELETING OPENING: %v", err)
+		return
+	}
+	defaultMessages.SendSuccess(context, "Opening deleted successfully", true)
 }
